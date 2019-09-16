10c0bf61f48af703c765c3dce1f16f0d3b01bd9e
Rodrigo Ruas <rodrigoruas2@gmail.com>
2019-09-16 09:47:52 -0300

    First Simple Methods

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -2,4 +2,16 @@ defmodule Cards do
   def hello do
     "hi there"
   end
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck,hand)
+  end
+
 end
